ca6a2213bceff059acf5b608cde0a81d354ed180
Ken <kendo302003@gmail.com>
2022-12-20 14:45:38 +0100

    adding navbar links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,6 +4,14 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <img src={ menuButton } />
+      <div className="nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }
