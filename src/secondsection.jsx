d0baf3d12e9ab827ab8b1b44c4eb04660c036658
Ken <kendo302003@gmail.com>
2022-12-21 15:22:54 +0100

    blog1 flex

--- src/secondsection.jsx
+++ src/secondsection.jsx
@@ -3,17 +3,18 @@ import blogImage from './assets/blog post image.png'
 const SecondSection = () => {
   return (
     <div className="second-head">
-      <h1>Recent Blogs</h1>
-      
-      <div className="Blog1">
+      <h1 className='sechead' >Recent Blogs</h1>
+
+      <div className="Blog Blog1">
         <img src={ blogImage }/>
         <div className="Blog1text">
           <p>Productivity</p>
           <h1>The Daily Productivity Planner</h1>
           <p>I have some big news! I've created a daily planner that will help you be more mindful and intentional throughout your day</p>
-          <button><a href="#">Read</a></button>
+          <a href="#"><button>Read</button></a>
         </div>
       </div>
+      <hr />
     </div>
   );
 }
