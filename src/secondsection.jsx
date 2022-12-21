5a78d31c84afa80508e7224fa211dfbfe802daef
Ken <kendo302003@gmail.com>
2022-12-21 15:14:16 +0100

    adding blog test

--- src/secondsection.jsx
+++ src/secondsection.jsx
@@ -4,8 +4,15 @@ const SecondSection = () => {
   return (
     <div className="second-head">
       <h1>Recent Blogs</h1>
+      
       <div className="Blog1">
         <img src={ blogImage }/>
+        <div className="Blog1text">
+          <p>Productivity</p>
+          <h1>The Daily Productivity Planner</h1>
+          <p>I have some big news! I've created a daily planner that will help you be more mindful and intentional throughout your day</p>
+          <button><a href="#">Read</a></button>
+        </div>
       </div>
     </div>
   );
