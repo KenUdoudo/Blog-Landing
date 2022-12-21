a178c85d3f73c5d20e39187223b57abd746cbe47
Ken <kendo302003@gmail.com>
2022-12-21 15:10:37 +0100

    importing image

--- src/secondsection.jsx
+++ src/secondsection.jsx
@@ -1,7 +1,12 @@
+import blogImage from './assets/blog post image.png'
+
 const SecondSection = () => {
   return (
     <div className="second-head">
       <h1>Recent Blogs</h1>
+      <div className="Blog1">
+        <img src={ blogImage }/>
+      </div>
     </div>
   );
 }
