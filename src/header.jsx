8ef12e70cb4e4852fd64f2f30cca1f2681f7c53b
Ken <kendo302003@gmail.com>
2022-12-21 11:52:30 +0100

    adding bottom-content

--- src/header.jsx
+++ src/header.jsx
@@ -5,6 +5,16 @@ const Header = () => {
       <h1>The Art of Self Care</h1>
       <p>"If YOU HAVE THE ABILITY TO LOVE, LOVE YOURSELF FIRST"</p>
       </div>
+      <div className="bottom-header">
+        <hr />
+        <ul>
+          <li><a href="#">Archives</a></li>
+          <li><a href="#"> Selfcare project</a></li>
+          <li><a href="#">Stress</a></li>
+          <li><a href="#">Relief</a></li>
+        </ul>
+        <hr />
+      </div>
     </div>
   );
 }
