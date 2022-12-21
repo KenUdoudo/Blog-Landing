63057eeb4b1750355a6894bf6f116005630de2a3
Ken <kendo302003@gmail.com>
2022-12-21 11:36:19 +0100

    content.css

--- src/header.jsx
+++ src/header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1>The Art of Self Care</h1>
-      <h2>"If YOU HAVE THE ABILITY TO LOVE, LOVE YOURSELF FIRST"</h2>
+      <p>"If YOU HAVE THE ABILITY TO LOVE, LOVE YOURSELF FIRST"</p>
     </div>
   );
 }
