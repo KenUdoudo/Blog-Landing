5896f2a7724bb98b1281790bc6a7854896d5fac2
Ken <kendo302003@gmail.com>
2022-12-21 18:15:18 +0100

    adding other blogs

--- src/secondsection.jsx
+++ src/secondsection.jsx
@@ -5,15 +5,42 @@ const SecondSection = () => {
     <div className="second-head">
       <h1 className='sechead' >Recent Blogs</h1>
 
-      <div className="Blog Blog1">
-        <img src={ blogImage }/>
-        <div className="Blog1text">
-          <p>Productivity</p>
-          <h1>The Daily Productivity Planner</h1>
-          <p>I have some big news! I've created a daily planner that will help you be more mindful and intentional throughout your day</p>
-          <a href="#"><button>Read</button></a>
+      <div className="blogflex">
+        <div className="Blog Blog1">
+          <img src={ blogImage }/>
+          <div className="Blog1text">
+            <p>Productivity</p>
+            <h1>The Daily Productivity Planner</h1>
+            <p>I have some big news! I've created a daily planner that will help you be more mindful and intentional throughout your day</p>
+            <a href="#"><button>Read</button></a>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="Blog Blog2">
+          <img src={ blogImage }/>
+          <div className="Blog1text">
+            <p>Productivity</p>
+            <h1>The Daily Productivity Planner</h1>
+            <p>I have some big news! I've created a daily planner that will help you be more mindful and intentional throughout your day</p>
+            <a href="#"><button>Read</button></a>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="Blog Blog3">
+          <img src={ blogImage }/>
+          <div className="Blog1text">
+            <p>Productivity</p>
+            <h1>The Daily Productivity Planner</h1>
+            <p>I have some big news! I've created a daily planner that will help you be more mindful and intentional throughout your day</p>
+            <a href="#"><button>Read</button></a>
+          </div>
         </div>
       </div>
+      
       <hr />
     </div>
   );
