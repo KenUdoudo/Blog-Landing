a8bb0d7e73fe4efe29cd68f8d2128e6575a422ea
Ken <kendo302003@gmail.com>
2022-12-22 07:15:36 +0100

    adding footer components

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-main">
-        
+        <h1>Newsletter</h1>
+        <form>
+          <input type="text" placeholder='FIRST NAME' required/>
+          <input type="email" placeholder='EMAIL' required/>
+          <button type="submit">Go</button>
+        </form>
       </div>
     </div>
   );
