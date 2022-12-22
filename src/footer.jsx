0e2b4985eabf41acdc558d4f457bb186f333a8d4
Ken <kendo302003@gmail.com>
2022-12-22 12:46:10 +0100

    input id

--- src/footer.jsx
+++ src/footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <div className="footer-main">
         <h1>Newsletter</h1>
         <form>
-          <input type="text" placeholder='FIRST NAME' required/>
-          <input type="email" placeholder='EMAIL' required/>
+          <input type="text" placeholder='FIRST NAME' id="first-input" required/>
+          <input type="email" placeholder='EMAIL' id="second-input" required/>
           <button type="submit" id='submitbtn'>Go</button>
         </form>
       </div>
