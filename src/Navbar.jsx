170a666c2e130ca6b4607abdaa0dd5a884993f41
Ken <kendo302003@gmail.com>
2022-12-29 09:42:00 +0100

    adding nav social

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,9 @@
 import menuButton from "./assets/menu icon svg.png";
 import { useState } from "react";
-
+import Facebook from './assets/surface1fb.png'
+import Twitter from './assets/surface1tweet.svg'
+import Indeed from './assets/surface1indeed.png'
+import d from './assets/surface1d.png'
 const Navbar = () => {
 
   
@@ -17,6 +20,12 @@ const Navbar = () => {
           <li><a href="#">Blog</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
+      </div>
+      <div className="navlinks">
+        <a href="#"><img src={ Facebook }/></a>
+          <a href="#"><img src={ Twitter }/></a>
+          <a href="#"><img src= { Indeed } /></a>
+          <a href="#"><img src= { d } /></a>
       </div>
     </div>
   );
